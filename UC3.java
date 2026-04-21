63a8b30b1dcbcbee4c262fb1ac40005f7ecd417d
Vriddhi Dhingra <vriddhidhingra@Vriddhis-MacBook-Air-2.local>
2026-04-21 09:22:53 +0530

    UC3 done

--- UC3.java
+++ UC3.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class UC3 {
 
     public static void main(String[] args) {
-
         int slot = getUserSlot();
         System.out.println("Slot entered: " + slot);
+
+
 
     }
 
     static int getUserSlot() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter slot number (1-9): ");
+        System.out.println("Enter slot number (1-9): ");
         int slot = sc.nextInt();
+        sc.close();
         return slot;
     }
 }
