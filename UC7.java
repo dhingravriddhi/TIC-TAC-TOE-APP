2f9b40639e062cd0aaa51fdf29bf4127b562fe2c
Vriddhi Dhingra <vriddhidhingra@Vriddhis-MacBook-Air-2.local>
2026-04-29 14:49:13 +0530

    UC7 done

---
+++ UC7.java
@@ -0,0 +1,61 @@
+import java.util.Random;
+
+public class UC7 {
+
+    static char[][] board = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+
+    public static void main(String[] args) {
+        computerMove();
+        printBoard();
+    }
+
+    static boolean isValidMove(int row, int col) {
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+        if (board[row][col] != ' ') {
+            return false;
+        }
+        return true;
+    }
+
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
+    }
+
+    static void computerMove() {
+        Random rand = new Random();
+        while (true) {
+            int slot = rand.nextInt(9) + 1;
+            int row = getRowFromSlot(slot);
+            int col = getColFromSlot(slot);
+            if (isValidMove(row, col)) {
+                placeMove(row, col, 'O');
+                break;
+            }
+        }
+    }
+
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) System.out.print(" | ");
+            }
+            System.out.println();
+            if (i < 2) System.out.println("---------");
+        }
+    }
+}
