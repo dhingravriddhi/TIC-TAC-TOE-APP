fff4633bfc1eae1b440d5fc8038b4147d3d33d49
Vriddhi Dhingra <vriddhidhingra@Vriddhis-MacBook-Air-2.local>
2026-04-29 14:51:32 +0530

    UC10 done

---
+++ UC10.java
@@ -0,0 +1,23 @@
+public class UC10 {
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        initializeBoard();
+        System.out.println(isDraw());
+    }
+
+    static void initializeBoard() {
+        for (int r = 0; r < 3; r++)
+            for (int c = 0; c < 3; c++)
+                board[r][c] = ' ';
+    }
+
+    static boolean isDraw() {
+        for (int r = 0; r < 3; r++)
+            for (int c = 0; c < 3; c++)
+                if (board[r][c] == ' ')
+                    return false;
+        return true;
+    }
+}
